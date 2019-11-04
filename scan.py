cb7bc9a99d3213d3f4eca76d9e060598e393834c
mevlüt furkan demir <mevlut.demir4@ogrenci.selcuk.edu.tr>
2019-11-04 13:54:34 +0200

    add comment lines
    Pls add this fucking comment lines

--- scan.py
+++ scan.py
@@ -1,6 +1,8 @@
+#add library
 import scapy.all as scapy
 
+#scan method 
 def scan(ip):
-    scapy.arping(ip)
+    scapy.arping(ip)#scan come ip
 
-scan("192.168.0.1/24")
+scan("192.168.0.1/24") #call scan method with ip args 

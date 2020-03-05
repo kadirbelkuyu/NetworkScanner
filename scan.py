d78b93a00275e282a3780a65cd2976120446cf4c
kadirbelkuyu <email>
2020-03-05 09:42:28 +0300

    better than before but still missing

--- scan.py
+++ scan.py
@@ -1,6 +1,16 @@
 import scapy.all as scapy
+import pprint
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answerd_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+
+    for element in answerd_list:
+        print(element)
+        print("------------------------------------------------------")
+
+    
 
 scan("192.168.0.1/24")

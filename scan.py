8ea39318f9f814449ca821f3bd0fc8bffee67a93
kadirbelkuyu <email>
2020-03-05 10:25:20 +0300

    As it is over

--- scan.py
+++ scan.py
@@ -1,16 +1,33 @@
+#!/usr/bin/env python
 import scapy.all as scapy
-import pprint
+import optparse
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target", help="Target IP / IP range.")
+    options, arguments = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please specify an IP adress, use --help for more info")
+    return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answerd_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    clients_list = []
+    for i in answered_list:
+        client_dict = {"ip": i[1].psrc, "mac": i[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 
-    for element in answerd_list:
-        print(element)
-        print("------------------------------------------------------")
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n-------------------------------------")
+    for j in results_list:
+        print(j["ip"] + "\t\t" + j["mac"])
 
-    
 
-scan("192.168.0.1/24")
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
